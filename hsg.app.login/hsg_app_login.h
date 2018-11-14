c32133722b156361041dd121b3af2dfc84f8a7f3
huos3203 <724987481@qq.com>
2018-11-14 11:53:15 +0800

    Initial Commit

---
+++ hsg.app.login/hsg_app_login.h
@@ -0,0 +1,19 @@
+//
+//  hsg_app_login.h
+//  hsg.app.login
+//
+//  Created by admin on 2018/11/14.
+//  Copyright © 2018年 clcw. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for hsg_app_login.
+FOUNDATION_EXPORT double hsg_app_loginVersionNumber;
+
+//! Project version string for hsg_app_login.
+FOUNDATION_EXPORT const unsigned char hsg_app_loginVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <hsg_app_login/PublicHeader.h>
+
+
